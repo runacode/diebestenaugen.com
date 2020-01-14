4da920efa9f638494db24ba6b97d863dd2addeed
runacode <59107019+runacode@users.noreply.github.com>
2020-01-13 20:30:48 -0600

    Auto move

--- magic.php
+++ magic.php
@@ -5,8 +5,11 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 switch ($path) {
 
 
- 
+
     default :
+    case '/resources/js/kvalidator2.js':
+        require_once (dirname(__FILE__) . "/resources/js/kvalidator.js.php");
+        break;
         header("HTTP/1.0 404 Not Found");
 
         exit;
