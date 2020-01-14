f0c3fb4ca398b507beaec3a837a34adf5c99a565
runacode <59107019+runacode@users.noreply.github.com>
2020-01-13 21:24:48 -0600

    Auto move

--- includes/data.php
+++ includes/data.php
@@ -6,16 +6,4 @@ $product = $data->products[0];
 include_once(dirname(__FILE__) . '/../locale/languages.php');
 
 
-$product->name = T("<b>Feg Serum</b> - Eyelash Enhancer");
-$product->one_free = T("1 Free");
-$product->discount_expires = T("The discount expires in ");
-$product->discount = T("Discount");
-$product->excellent = T("Excellent");
-$product->num_reviews = T("53 Reviews");
-$product->buy_2_get_1 = T("Buy 2 and get 1 for free");
-$product->satisfaction_guaranteed = T("Satisfaction<br>Guaranteed");
-$product->add_to_cart = T("Add to Cart");
-$product->rand_head = T("USA Only Free Shipping Till the Weekend");
-$product->head = T("US 60% off + Free Shipping");
-
 ?>
